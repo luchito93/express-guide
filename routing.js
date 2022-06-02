04cd974e30b23e39efc0a33f8221d1c07f9e9aa3
Luis <luisgomez@unisangil.edu.co>
2022-06-02 07:16:20 -0500

    feat: :sparkles: Overriding express method and props

--- routing.js
+++ routing.js
@@ -49,12 +49,46 @@ app.get('/elmisi', (req, res) => {
     res.send(`el misi bonito a la hora de ${req.requestTime}`)
 })
 
-app.use(cookieParser()).use(validateCookiesMidd).use(errHandlerMidd)
+app.use(cookieParser())
 
 
-app.get('/elcookieparser', (req, res) => {
+app.get('/elcookieparser', [validateCookiesMidd], (req, res) => {
     // para usar se debe agregar el header Cookie testCookie=luchin;Greet=Hello
     res.send(`retornando el cookie parser ${JSON.stringify(req.cookies)}`)
+})
+
+// error handler for all endpoits avobe
+app.use(errHandlerMidd)
+
+app.get('/overriding-api-methond', (req, res) => {
+    res.sendStatus(401, 'application/json', '{"error": "no esta autorizado criatura"}' )
+})
+
+app.get('/overriding-api-prop', (req, res) => {
+    res.json({ propIpOverriding: req.ip, header: req.headers['client-ip'] })
+})
+
+//Overriding express api method
+app.response.sendStatus = function (statusCode, type, message) {
+    return this.contentType(type).status(statusCode).send(message)
+}
+// Overridin express api properties
+Object.defineProperty(app.request, 'ip', {
+    configurable: true,
+    enumerable: true,
+    /**
+     * el getter del accessor se puede escribir como get () {  } 
+     * utilizando el short had method definition
+     * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Method_definitions
+     * 
+     * NOta: no se puede escribir con arrow funcion porque se necesita el bind del this que esta
+     * bien afuera, que en este caso el this seria app.request
+     */
+    get: function () {
+        // cambia la propiedad ip por la que venga en el header
+        // tambien se puede escribir asi this.headers['client-ip']
+        return this.get('Client-IP') 
+    }
 })
 
 app.listen(port, () => {
